cdcacb87004b46f1365d03ebce81bae94b286955
Lázár Marcell <marcell.lazar2003@gmail.com>
2022-11-16 14:41:20 +0100

    Add project files.

---
+++ ProgrammingCourse/CourseData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingCourse
+{
+    class CourseData
+    {
+        public string name;
+        public char gender;
+        public int code;
+        public int paidSum;
+        public Dictionary<string, int> results = new Dictionary<string, int>();
+
+        public CourseData(string row)
+        {
+            List<string> temp = row.Split(';').ToList();
+
+            this.name = temp[0];
+            this.gender = Convert.ToChar(temp[1]);
+            this.code = Convert.ToInt32(temp[2]);
+            this.paidSum = Convert.ToInt32(temp[3].TrimStart('$'));
+            this.results.Add("prog", Convert.ToInt32(temp[4]));
+            this.results.Add("graf", Convert.ToInt32(temp[5]));
+            this.results.Add("architekt", Convert.ToInt32(temp[6]));
+            this.results.Add("mestint", Convert.ToInt32(temp[7]));
+        }
+    }
+}
---
+++ ProgrammingCourse/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingCourse
+{
+    class Program
+    {
+        static List<CourseData> data = new List<CourseData>();
+
+        static void Main(string[] args)
+        {
+            MasodikFeladat();
+            HarmadikFeladat();
+            NegyedikFeladat();
+            OtodikFeladat();
+
+            Console.ReadKey();
+        }
+
+        private static void OtodikFeladat()
+        {
+
+        }
+
+        private static void NegyedikFeladat()
+        {
+            double atlag1 = (from d in data where d.gender == 'm' select d.results.Values.Average()).ToList().Average();
+            double atlag2 = (from d in data where d.gender == 'f' select d.results.Values.Average()).ToList().Average();
+            Console.WriteLine($"4. feladat:\n" +
+                $"\tFiúk   átlagteljesítménye: {Math.Round(atlag1, 2)}%\n" +
+                $"\tLányok átlagteljesítménye: {Math.Round(atlag2, 2)}%");
+        }
+
+        private static void HarmadikFeladat()
+        {
+            Console.WriteLine($"3. feladat:\n" +
+                $"\tA tanfolyamra {data.Count} fő iratkozott be.");
+        }
+
+        private static void MasodikFeladat()
+        {
+            using (StreamReader sr = new StreamReader("coursedata.csv"))
+            {
+                while (!sr.EndOfStream)
+                {
+                    data.Add(new CourseData(sr.ReadLine()));
+                }
+            }
+        }
+    }
+}
